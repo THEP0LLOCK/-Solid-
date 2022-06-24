539ab85e15fd8c529548a2ae390e5ed5df68fe20
THEP0LLOCK <63382032+THEP0LLOCK@users.noreply.github.com>
2022-06-25 00:13:07 +0900

    Update

--- substitit/js/greetings.js
+++ substitit/js/greetings.js
@@ -13,6 +13,9 @@ function onLoginSubmit(event) {
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
     localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings(username);
+}
+function paintGreetings(username){
     greeting.innerText = `Hello ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
@@ -22,9 +25,10 @@ loginForm.addEventListener("submit", onLoginSubmit);
 
 //tmp
 const savedUsername = localStorage.getItem("USERNAME_KEY");
+console.log(savedUsername);
 if (savedUsername === null) {
     loginForm.classList.remove(HIDDEN_CLASSNAME);
 }
 else {
-    greeting.classList.remove(HIDDEN_CLASSNAME);
+    paintGreetings(savedUsername);
 }
